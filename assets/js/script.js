aca23a6f4b4f1986411ee1d40c6b7dde03d82e6b
Haguermeister <cadethague70@gmail.com>
2022-01-14 11:43:41 -0500

    Added pull of drink recipes and logic to make all mixins in one array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+var mixins = JSON.parse(localStorage.getItem("mixins"));
+var drinkRecipes = JSON.parse(localStorage.getItem("drinkRecipes"));
+if (mixins == null) {
+    mixins = [];
+    grabMixins();
+}
+if (drinkRecipes == null) {
+    drinkRecipes = [];
+    grabDrinks();
+}
 function grabMixins() {
+    console.log("Mixins Api");
     for (var i = 1; i < 580; i++) {
         var mixinApiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=" + i;
         fetch(mixinApiUrl).then(function (response) {
@@ -13,15 +24,48 @@ function grabMixins() {
     }
 }
 function grabDrinks() {
+    console.log("Drinks Api");
     alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     for (var i = 0; i < alphabet.length; i++) {
         var drinkApiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + alphabet[i];
         fetch(drinkApiUrl).then(function (response) {
             response.json().then(function (data) {
-
+                if (data.drinks !== null) {
+                    for (var i = 0; i < data.drinks.length; i++) {
+                        drinkRecipes.push(data.drinks[i]);
+                    }
+                }
             }
             );
         });
     }
+    parseIng();
 }
-console.log(mixins);
+function parseIng() {
+    for (var i = 0; i < drinkRecipes.length; i++) {
+        var obj = drinkRecipes[i];
+        var values = [];
+        for (var j = 1; j < 16; j++) {
+            key = "strIngredient" + j;
+            if (obj[key] !== null) {
+                values.push(obj[key]);
+            }
+        }
+        drinkRecipes[i].ingArray = values;
+    }
+    localStorage.setItem("drinkRecipes", JSON.stringify(drinkRecipes));
+}
+function findMixinIndex(mixin) {
+    var ref = [],
+        indexMixin = [];
+    for (var i = 0; i < drinkRecipes.length; i++) {
+        indexMixin[i] = drinkRecipes[i].ingArray.some(ing => ing == mixin);
+    }
+    for (var i = 0; i < indexMixin.length; i++) {
+        if (indexMixin[i]) {
+            ref.push(i);
+        }
+    }
+    return ref;
+}
+var ref = findMixinIndex("Egg");
