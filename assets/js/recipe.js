7eae120ab47d38517debb4f14d30f0a40ccd2501
Haguermeister <cadethague70@gmail.com>
2022-01-18 23:02:09 -0600

    Finished display drinks js

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,11 +1,15 @@
 var drinkValue = JSON.parse(localStorage.getItem("drinkValue"));
 var drinkRecipes = JSON.parse(localStorage.getItem("drinkRecipes"));
 var index = drinkRecipes.findIndex(array => array.strDrink == drinkValue);
-console.log(drinkRecipes[index]);
-$(".drink-image").attr(".src", drinkRecipes[index].strDrinkThumb);
+$(".drink-image").attr("src", drinkRecipes[index].strDrinkThumb);
 $(".drink-title").text(drinkValue);
-for (var j = 1; j < drinkRecipes.ingArray.length + 1; j++) {
-    keyIng = "strIngredient" + j;
-    keyMeas = "strMeasure" + j;
-    $(".drink-ingriedients").append($("li").text((drinkRecipes[index][key]) + (drinkRecipes[index][keyMeas])))
+for (var j = 1; j < drinkRecipes[j].ingArray.length; j++) {
+    var keyIng = "strIngredient" + j;
+    var keyMeas = "strMeasure" + j;
+    $(".drink-measurements").append($("<li>").text((drinkRecipes[index][keyIng]) + " : " + (drinkRecipes[index][keyMeas])))
+}
+var steps = drinkRecipes[index].strInstructions.split(".");
+console.log(steps);
+for (var i = 0; i < steps.length - 1; i++) {
+    $(".mixing-instructions").append($("<li>").text(steps[i]));
 }
