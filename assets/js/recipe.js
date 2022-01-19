c33545bb2f5fb88d6fd37ec1ec0569ee71285ec0
Haguermeister <cadethague70@gmail.com>
2022-01-18 22:05:38 -0600

    fixed index.

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,4 +1,4 @@
 var drinkShow = JSON.parse(localStorage.getItem("drinkShow"));
 var drinkRecipes = JSON.parse(localStorage.getItem("drinkRecipes"));
-var index = drinkRecipes.findIndex(array => array.strDrink == drinkRecipes);
+var index = drinkRecipes.findIndex(array => array.strDrink == drinkShow);
 console.log(index);
